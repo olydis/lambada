dbf2c37ffcec8cca3191197ae32a03960d893b1e
Johannes <jogus@live.de>
2015-04-19 20:07:32 +0200

    runtime.hs finished, added parser

--- runtime/haskell/src/runtime.hs
+++ runtime/haskell/src/runtime.hs
@@ -20,19 +20,43 @@ reduce (App f a)                 = fmap (\f' -> App f' a) (reduce f)
 reduce _                         = Nothing
 
 
--- Playground
-type Env = String -> Maybe Expression
+-- Environment
+
+type Env = [(String, Expression)]
 
 envInitial :: Env
-envInitial "u" = Just U
-envInitial _   = Nothing
+envInitial = [("u", U)]
 
 envAdd :: String -> Expression -> Env -> Env
-envAdd name value env query = if query == name then Just value else env query
+envAdd name value []           = [(name, value)]
+envAdd name value ((a, b):env) = if a == name then ((a, value):env) else (a, b):envAdd name value env
+
+envGet :: String -> Env -> Expression
+envGet name []           = error $ "no definition for " ++ name ++ " found"
+envGet name ((a, b):env) = if a == name then b else envGet name env
 
 envParse :: String -> Env -> Env
-envParse 
+envParse s env = envParseBag [] Nothing Nothing s env
 
-i = App U U
-k = App U (App U i)
-s = App U k
+envParseBag :: [Expression] -> Maybe String -> Maybe String -> String -> Env -> Env
+envParseBag []      Nothing  _        []           env = env
+envParseBag _       _        _        []           env = error "unexpected end of input"
+envParseBag stack   name     Nothing  (' '  : s)   env = envParseBag stack name Nothing s env
+envParseBag stack   name     Nothing  ('\n' : s)   env = envParseBag stack name Nothing s env
+envParseBag stack   name     Nothing  ('\r' : s)   env = envParseBag stack name Nothing s env
+envParseBag []      _        Nothing  ('.'  : s)   env = error "unexpected application (stack was empty)"
+envParseBag [e]     (Just n) Nothing  ('.'  : s)   env = envParseBag [] Nothing Nothing s (envAdd n e env)
+envParseBag (b:a:e) name     Nothing  ('.'  : s)   env = envParseBag (App a b : e) name Nothing s env
+ 
+envParseBag stack   Nothing  (Just x) t@(' '  : s) env = envParseBag stack (Just x) Nothing t env
+envParseBag stack   Nothing  (Just x) t@('\n' : s) env = envParseBag stack (Just x) Nothing t env
+envParseBag stack   Nothing  (Just x) t@('\r' : s) env = envParseBag stack (Just x) Nothing t env
+envParseBag stack   Nothing  (Just x) t@('.'  : s) env = envParseBag stack (Just x) Nothing t env 
+
+envParseBag stack   name     (Just x) t@(' '  : s) env = envParseBag (envGet x env : stack) name Nothing t env
+envParseBag stack   name     (Just x) t@('\n' : s) env = envParseBag (envGet x env : stack) name Nothing t env
+envParseBag stack   name     (Just x) t@('\r' : s) env = envParseBag (envGet x env : stack) name Nothing t env
+envParseBag stack   name     (Just x) t@('.'  : s) env = envParseBag (envGet x env : stack) name Nothing t env 
+
+envParseBag stack   name     Nothing  t@(c : s)    env = envParseBag stack name (Just "") t env
+envParseBag stack   name     (Just x) (c : s)      env = envParseBag stack name (Just (x ++ [c])) s env
