54c90b5e6f758a9898888d3bf98137cb869fa264
Johannes Bader <jogus@live.de>
2019-11-25 11:02:26 -0800

    simplified tracing

--- runtime/compiled_javascript/working_tracing.js
+++ runtime/compiled_javascript/working_tracing.js
@@ -24,62 +24,59 @@ const lazy = name => f => {
 // const k = lazy(["k", ["u", ["u", "i"]]])(() => u("k0")(lazy("k1")(() => u("k10")(i))));
 // const s = lazy(["s", ["u", "k"]])(() => u("s0")(k));
 
-const norm = e => (Array.isArray(e[0]) ? e[0].concat(e.slice(1)) : e);
+const norm = e => (Array.isArray(e[0]) ? norm(e[0].concat(e.slice(1))) : e);
 const hasNull = msg =>
   msg === null || (Array.isArray(msg) && msg.some(hasNull));
+const print = (e, p = true) =>
+  Array.isArray(e) ? (x => (p ? `(${x})` : x))(e.map(print).join(" ")) : e;
 const trace = msg =>
-  (msg => (hasNull(msg) || console.error(msg), msg))(norm(msg));
+  (msg => (hasNull(msg) || console.error(print(msg, false)), msg))(norm(msg));
+const trace2 = (subst, e) => trace(subst.concat(e.slice(1)));
 
-// const i = (trace, e) => a => a(trace, trace(e.slice(1)));
-const _k = (trace, e) => a => b => a(trace, trace([e[1]].concat(e.slice(3))));
-const _s = (trace, e) => a => b => c =>
-  a(trace, trace([e[1], e[3], [e[2], e[3]]].concat(e.slice(4))))(c)(
-    (trace, e) => b(trace, e)(c)
-  );
-const u = (trace, e) => x =>
-  x(trace, trace([e[1], /*S*/ null, /*K*/ null].concat(e.slice(2))))(_s)(_k);
+// const i = (e) => a => a(trace(e.slice(1)));
+const _k = e => a => b => a(trace([e[1]].concat(e.slice(3))));
+const _s = e => a => b => c =>
+  a(trace([e[1], e[3], [e[2], e[3]]].concat(e.slice(4))))(c)(e => b(e)(c));
+const u = e => x =>
+  x(trace([e[1], /*S*/ null, /*K*/ null].concat(e.slice(2))))(_s)(_k);
 
 // Note: it's okay for `u` to strictly evaluate its argument "x" (e.g. to see what it is),
 //       since it becomes the head of "x S K" anyways!
 // const tag = (tag, f) => ((f.tag = tag), f);
-// const u = (trace, e) => x =>
+// const u = (e) => x =>
 //   x === u
-//     ? tag("i", y => y(trace, trace(e.slice(2))))
+//     ? tag("i", y => y(trace(e.slice(2))))
 //     : ((y, e) =>
 //         y.tag === "i"
-//           ? tag("sk", y => z => z(trace, trace(e.slice(2))))
+//           ? tag("sk", y => z => z(trace(e.slice(2))))
 //           : y.tag === "sk"
-//           ? tag("k", a => b => a(trace, trace([e[0]].concat(e.slice(2)))))
+//           ? tag("k", a => b => a(trace([e[0]].concat(e.slice(2)))))
 //           : y.tag === "k"
 //           ? tag("s", a => b => c =>
-//               a(trace, trace([e[0], e[2], [e[1], e[2]]].concat(e.slice(3))))(c)(
-//                 (trace, e) => b(trace, e)(c)
+//               a(trace([e[0], e[2], [e[1], e[2]]].concat(e.slice(3))))(c)(
+//                 (e) => b(e)(c)
 //               )
 //             )
-//           : x(trace, trace([e[1], "S", "K"].concat(e.slice(2))))(s)(k))(
-//         x(trace, [e[1], "STRICT"]),
+//           : x(trace([e[1], "S", "K"].concat(e.slice(2))))(s)(k))(
+//         x([e[1], "STRICT"]),
 //         e.slice(2)
 //       );
 // Note: The above is bad since a) it results in funky traces due to out of order eval, b) can never be complete that way
 
-const i = (trace, e) => u(trace, trace(["u", "u"].concat(e.slice(1))))(u);
-const k = (trace, e) =>
-  u(trace, trace(["u", ["u", "i"]].concat(e.slice(1))))((trace, e) =>
-    u(trace, e)(i)
-  );
-const s = (trace, e) => u(trace, trace(["u", "k"].concat(e.slice(1))))(k);
-const m = (trace, e) =>
-  s(trace, trace(["s", "i", "i"].concat(e.slice(1))))(i)(i);
+const i = e => u(trace2(["u", "u"], e))(u);
+const k = e => u(trace2(["u", ["u", "i"]], e))(e => u(e)(i));
+const s = e => u(trace2(["u", "k"], e))(k);
+const m = e => s(trace2(["s", "i", "i"], e))(i)(i);
 
 // Code gen end
 
-s(trace, trace(["s", "k", "k", "k"]))(k)(k)(k);
+s(trace(["s", "k", "k", "k"]))(k)(k)(k);
 console.log();
-s(trace, trace(["s", "i", "k", "i", "i"]))(i)(k)(i)(i);
+s(trace(["s", "i", "k", "i", "i"]))(i)(k)(i)(i);
 console.log();
-i(trace, trace(["i", "i", "s"]))(i)(s);
+i(trace(["i", "i", "s"]))(i)(s);
 console.log();
-k(trace, trace(["k", "i", "k", "s"]))(i)(k)(s);
+k(trace(["k", "i", "k", "s"]))(i)(k)(s);
 console.log();
-m(trace, trace(["m", "k"]))(k);
+m(trace(["m", "k"]))(k);
 process.exit(0);
